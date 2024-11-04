83bba528f7207536a2a75c7f292e63f2fc640a56
Mercedes Chase-Kelly <mercedestck@gmail.com>
2024-11-04 02:34:49 +0000

    Place a border around the welcome message

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -3,6 +3,9 @@ function welcomeMessage() {
     if (message.style.display === 'none') {
         message.style.display = 'block';
         message.style.marginTop = '0';
+        message.style.border = '2px solid black';
+        message.style.width = '205px';
+        message.style.textAlign = 'center';
     } else {
         message.style.display = 'none';
     }
