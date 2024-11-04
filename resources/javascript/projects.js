a9873c4433f55cf8e46278fec8351b45df648975
Mercedes Chase-Kelly <mercedestck@gmail.com>
2024-11-04 23:14:42 +0000

    Turn all projects in list to buttons, and create functions to hide and show the content of each project

--- resources/javascript/projects.js
+++ resources/javascript/projects.js
@@ -3,3 +3,25 @@ function toggleImageText() {
     document.getElementById('project-one').classList.toggle('hideDetails');
 }
 
+function toggleImageTextTwo() {
+    document.getElementById('project-two').classList.toggle('hideDetails');
+}
+
+function toggleImageTextThree() {
+    document.getElementById('project-three').classList.toggle('hideDetails');
+}
+
+function toggleImageTextFour() {
+    document.getElementById('project-four').classList.toggle('hideDetails');
+}
+
+function toggleImageTextFive() {
+    document.getElementById('project-five').classList.toggle('hideDetails');
+}
+
+function toggleImageTextSix() {
+    document.getElementById('project-six').classList.toggle('hideDetails');
+}
+
+
+
