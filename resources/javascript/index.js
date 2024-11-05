72e15e4083000a2136f2deff4cdd9ce6b6ec1ac6
Mercedes Chase-Kelly <mercedestck@gmail.com>
2024-11-05 23:21:28 +0000

    Complete styling for homepage

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -3,11 +3,12 @@ function welcomeMessage() {
     if (message.style.display === 'none') {
         message.style.display = 'block';
         message.style.marginTop = '0';
-        message.style.border = '2px solid black';
+        /*message.style.backgroundColor = '#C1B8AE';*/
+        message.style.backgroundColor = '#FFF';
+        message.style.border = '2px solid #8ABADD';
         message.style.width = '205px';
         message.style.textAlign = 'center';
     } else {
         message.style.display = 'none';
     }
 }
-
