a9c0e8bf5718cb3ba14874cdc759760de52de2d7
Mercedes Chase-Kelly <mercedestck@gmail.com>
2024-11-09 12:25:07 +0000

    Clean up file

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -1,14 +1,11 @@
 function welcomeMessage() {
     const message = document.getElementById('welcomeMessage');
-    
         message.style.display = 'block';
         message.style.marginTop = '0';
-        /*message.style.backgroundColor = '#C1B8AE';*/
         message.style.backgroundColor = '#FFF';
         message.style.border = '3px solid #8ABADD';
         message.style.width = '205px';
         message.style.textAlign = 'center';
-
 }
 
 function exitWelcomeMessage() {
